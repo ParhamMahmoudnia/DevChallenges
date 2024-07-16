579cbd25e7ef8ab6c370f2a1de5dd95f0055b56f
parham-mahmodnia <parham.mahmoudnia@gmail.com>
2024-07-17 01:48:10 +0330

    "Improved IsPrime method for efficient prime number checking."

---
+++ PrimeNumber/Prime.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    public class Prime
+    {
+        /// <summary>
+        /// Checks if a given number is a prime number.
+        /// </summary>
+        /// <param name="number">The number to check.</param>
+        /// <returns>True if the number is prime, otherwise false.</returns>
+        public static bool IsPrime(int number)
+        {
+            // 1 and numbers less than or equal to 1 are not prime
+            if (number <= 1)
+                return false;
+
+            // 2 is a prime number
+            if (number == 2)
+                return true;
+
+            // Even numbers greater than 2 are not prime
+            if (number % 2 == 0)
+                return false;
+
+            // Check for odd factors from 3 up to the square root of the number
+            // We increment by 2 to check only odd numbers
+            for (int i = 3; i * i <= number; i += 2)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- PrimeNumber/Program.cs
+++ PrimeNumber/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number to check if it is prime: ");
+            int input = int.Parse(Console.ReadLine());
+
+            bool isPrime = Prime.IsPrime(input);
+
+            Console.WriteLine($"Is {input} a prime number? {isPrime}");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();  // Wait for user input before closing console
+        }
+    }
+}
