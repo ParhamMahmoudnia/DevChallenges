72ed14991ecbc86dd0e78c8ce78d9ebc09d37d5d
parham-mahmodnia <parham.mahmoudnia@gmail.com>
2024-07-17 00:24:21 +0330

    "Added Fibonacci sequence generator."

---
+++ FibonacciSequence/Fibonacci.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+
+namespace FibonacciSequence
+{
+    public class Fibonacci
+    {
+        /// <summary>
+        /// Generates a Fibonacci sequence of a given length.
+        /// </summary>
+        /// <param name="length">The length of the Fibonacci sequence to generate.</param>
+        /// <returns>A string representation of the Fibonacci sequence.</returns>
+        public static string GenerateFibonacci(int length)
+        {
+            // Initialize the array to hold the Fibonacci sequence values
+            BigInteger[] fibonacciArray = new BigInteger[length];
+
+            // Set the first two values in the sequence
+            fibonacciArray[0] = 0;
+            fibonacciArray[1] = 1;
+
+            // Compute the remaining values in the sequence
+            for (int i = 2; i < length; i++)
+            {
+                fibonacciArray[i] = fibonacciArray[i - 1] + fibonacciArray[i - 2];
+            }
+
+            // Join the array values into a comma-separated string and return
+            return string.Join(", ", fibonacciArray);
+        }
+    }
+}
--- FibonacciSequence/Program.cs
+++ FibonacciSequence/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace FibonacciSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the length of Fibonacci sequence: ");
+            int length = int.Parse(Console.ReadLine());
+
+            // Generate Fibonacci sequence
+            string fibonacciSequence = Fibonacci.GenerateFibonacci(length);
+
+            // Display the Fibonacci sequence to the user
+            Console.WriteLine($"Fibonacci sequence of length {length}: {fibonacciSequence}");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();  // Wait for user input before closing console
+        }
+    }
+}
