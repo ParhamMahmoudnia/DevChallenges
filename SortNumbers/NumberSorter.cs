efb182dfa2e21b10b8bfec2a3ffc8da4679f8078
parham-mahmodnia <parham.mahmoudnia@gmail.com>
2024-07-17 23:29:42 +0330

    "Implement Bubble Sort algorithm for sorting an array of integers"

---
+++ SortNumbers/NumberSorter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortNumbers
+{
+    public class NumberSorter
+    {
+        // Bubble Sort algorithm to sort an array of numbers
+        public static int[] ReturnSorted(int[] numbers)
+        {
+            int n = numbers.Length;
+
+            // Outer loop for passes
+            for (int i = 0; i < n - 1; i++)
+            {
+                // Inner loop for comparisons
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    // Swap if the current element is greater than the next element
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                    }
+                }
+            }
+
+            return numbers;
+        }
+    }
+}
---
+++ SortNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace SortNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user for the input
+            Console.WriteLine("Please enter a series of integers separated by spaces:");
+            string input = Console.ReadLine();
+
+            // Split the input string into an array of strings
+            string[] inputStrings = input.Split(' ');
+
+            // Convert the array of strings to an array of integers
+            int[] numbers = Array.ConvertAll(inputStrings, int.Parse);
+
+            // Sort the numbers using the NumberSorter class
+            int[] sortedNumbers = NumberSorter.ReturnSorted(numbers);
+
+            // Print the sorted numbers
+            Console.WriteLine("The sorted list of numbers is:");
+            foreach (int number in sortedNumbers)
+            {
+                Console.Write(number + " ");
+            }
+
+            // Wait for user input before closing console
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
