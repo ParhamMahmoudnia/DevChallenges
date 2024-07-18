fc110db259feb67fa0bf6ff8abb8f6ac34973ce7
parham-mahmodnia <parham.mahmoudnia@gmail.com>
2024-07-18 21:18:08 +0330

    "Implemented Decimal to Binary conversion"

---
+++ DecimalToBinary/DecimalToBinaryConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecimalToBinary
+{
+    public class DecimalToBinaryConverter
+    {
+
+        public static string ConvertDecimalToBinary(int decimalNumber)
+        {
+            // If the input number is 0, return "0"
+            if (decimalNumber == 0)
+                return "0";
+
+            string binaryResult = string.Empty;
+            while (decimalNumber > 0)
+            {
+                // Find the remainder when decimalNumber is divided by 2
+                int remainder = decimalNumber % 2;
+
+                // Prepend the remainder to the result string
+                binaryResult = remainder + binaryResult;
+
+                // Divide the number by 2 for the next iteration
+                decimalNumber /= 2;
+            }
+
+            return binaryResult;
+        }
+}
---
+++ DecimalToBinary/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DecimalToBinary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user for the input
+            Console.WriteLine("Please enter a decimal number:");
+            int decimalInput = int.Parse(Console.ReadLine());
+
+            // Convert the decimal number to binary
+            string binaryOutput = DecimalToBinaryConverter.ConvertDecimalToBinary(decimalInput);
+
+            // Print the binary number
+            Console.WriteLine($"The binary equivalent of decimal {decimalInput} is: {binaryOutput}");
+
+            // Wait for user input before closing console
+            Console.ReadKey();
+        }
+    }
+}
